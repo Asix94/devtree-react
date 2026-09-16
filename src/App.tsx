eb710d542d588fd9703486edb6c772b7fc625506
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 13:08:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const holaMundo = "Hola Mundo desde TSX"
+
+  return (
+    <>
+      { holaMundo }
+    </>
+  )
+}
+
+export default App
