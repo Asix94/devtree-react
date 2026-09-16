7eb5ea5688e1824491e2651aac44c5ad2745a46e
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 13:52:36 +0200

    navegando entre paginas con react router

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom'
+
 export default function RegisterView() {
     return (
-        <div>RegisterView</div>
+        <>
+            <div>RegisterView</div>
+            
+            <nav>
+                <Link to='/auth/login'>
+                        Ya tienes una cuenta? Inicia Sesión
+                </Link>
+            </nav>
+        </>
+        
     )
 }
