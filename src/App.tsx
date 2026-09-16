78eb695a307c765135a82ea728cab02b43b2870b
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 13:20:18 +0200

    instalando tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  const holaMundo = "Hola Mundo desde TSX"
-
   return (
     <>
-      { holaMundo }
+      <h1>Hola Mundo</h1>
     </>
   )
 }
