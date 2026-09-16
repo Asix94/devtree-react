35ab541f9500c21acf56ca7727620802c8204f65
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 14:24:32 +0200

    Creando un layout principal para autenticar

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function RegisterView() {
     return (
-        <>
-            <div>RegisterView</div>
-            
+        <>            
             <nav>
                 <Link to='/auth/login'>
                         Ya tienes una cuenta? Inicia Sesión
