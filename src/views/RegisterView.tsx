6a5ec640459fa60254af9c974f644909413370c0
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 16:47:32 +0200

    Creando un compoenente para mostrar errores

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,7 +3,16 @@ import { useForm } from 'react-hook-form';
 import ErrorMessage from '../components/ErrorMessage';
 
 export default function RegisterView() {
-    const { register, watch, handleSubmit, formState: { errors }} = useForm();
+
+    const initialValues = {
+        name: '',
+        email: '',
+        handle: '',
+        password: '',
+        password_confirmation: ''
+    }
+
+    const { register, watch, handleSubmit, formState: { errors }} = useForm({defaultValues: initialValues});
 
     console.log(errors);
 
@@ -84,9 +93,10 @@ export default function RegisterView() {
                         placeholder="Repetir Password"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
                         {...register('password_confirmation', { 
-                            required: "El Password es obligatorio"
+                            required: "Repetir Password es obligatorio"
                         })}
                     />
+                    { errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage> }
                 </div>
 
                 <input
