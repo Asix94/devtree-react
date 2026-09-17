27ceacf2b97eea77a70025bf7bf08235a3434bdf
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-17 16:57:55 +0200

    enviando la peticion con el JWT en react query

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -3,9 +3,14 @@ import api from '../config/axios';
 
 
 export async function getUser() {
+    const token = localStorage.getItem('AUTH_TOKEN');
     try {
-       const { data } = await api('/user')
-       console.log(data);
+       const { data } = await api('/user', { 
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+       })
+       return data
     } catch(error) {
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
