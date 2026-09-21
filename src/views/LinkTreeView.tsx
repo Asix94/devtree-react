967dae096fe49d7748d6036412d44c3fcd80599b
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-21 12:14:17 +0200

    habilitando y deshabilitando enlaces de nuestro tree

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -7,7 +7,11 @@ export default function LinkTreeView() {
 
     const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
         const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link);
-        console.log(updatedLinks);
+        setDevTreeLinks(updatedLinks);
+    }
+
+    const handleEnableLink = (socialNetwork: string) => {
+        const updatedLinks = devTreeLinks.map(link => link.name === socialNetwork ? {...link, enabled: !link.enabled} : link);
         setDevTreeLinks(updatedLinks);
     }
 
@@ -18,6 +22,7 @@ export default function LinkTreeView() {
                     key={item.name}
                     item={item}
                     handleUrlChange={handleUrlChange}
+                    handleEnableLink={handleEnableLink}
                 />
             ))}
         </div>
