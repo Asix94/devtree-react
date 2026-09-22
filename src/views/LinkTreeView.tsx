fc0f44a479c55a63bb1499bb85be54c92dcfb297
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-22 11:46:59 +0200

    finalizando la logica del orden

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -25,7 +25,7 @@ export default function LinkTreeView() {
     useEffect(() => {
         const updatedData = devTreeLinks.map(item => {
             const userLink = JSON.parse(user.links).find((link: SocialNetwork) => link.name === item.name)
-            if(userLink) {
+            if (userLink) {
                 return { ...item, url: userLink.url, enabled: userLink.enabled }
             }
             return item
@@ -33,18 +33,18 @@ export default function LinkTreeView() {
         setDevTreeLinks(updatedData)
     }, []);
 
-    const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-        const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link);
+    const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? { ...link, url: e.target.value } : link);
         setDevTreeLinks(updatedLinks);
     }
 
-    const links : SocialNetwork[] = JSON.parse(user.links);
+    const links: SocialNetwork[] = JSON.parse(user.links);
 
     const handleEnableLink = (socialNetwork: string) => {
         const updatedLinks = devTreeLinks.map(link => {
-            if(link.name === socialNetwork) {
-                if(isValidUrl(link.url)) {
-                    return {...link, enabled: !link.enabled}
+            if (link.name === socialNetwork) {
+                if (isValidUrl(link.url)) {
+                    return { ...link, enabled: !link.enabled }
                 } else {
                     toast.error('URL no válida');
                 }
@@ -55,22 +55,37 @@ export default function LinkTreeView() {
 
         let updatedItems: SocialNetwork[] = []
         const selectedSocialNetwork = updatedLinks.find(link => link.name === socialNetwork)
-        if(selectedSocialNetwork?.enabled) {
-            const newItem = {
-                ...selectedSocialNetwork,
-                id: links.length + 1
+        if (selectedSocialNetwork?.enabled) {
+            const id = links.filter(link => link.id).length + 1
+            if (links.some(link => link.name === socialNetwork)) {
+                updatedItems = links.map(link => {
+                    if( link.name === socialNetwork) {
+                        return {
+                            ...link,
+                            enabled: true,
+                            id
+                        }
+                    } else {
+                        return link
+                    }
+                })
+            } else {
+                const newItem = {
+                    ...selectedSocialNetwork,
+                    id
+                }
+                updatedItems = [...links, newItem]
             }
-            updatedItems = [...links, newItem]
         } else {
             const indexTpUpdate = links.findIndex(link => link.name === socialNetwork)
             updatedItems = links.map(link => {
-                if(link.name === socialNetwork) {
+                if (link.name === socialNetwork) {
                     return {
                         ...link,
                         id: 0,
                         enabled: false
                     }
-                } else if(link.id > indexTpUpdate) {
+                } else if (link.id > indexTpUpdate) {
                     return {
                         ...link,
                         id: link.id - 1
@@ -79,10 +94,7 @@ export default function LinkTreeView() {
                     return link
                 }
             })
-            console.log(indexTpUpdate);
         }
-
-        console.log(updatedItems);
 
         queryClient.setQueryData(['user'], (prevData: User) => {
             return {
@@ -95,14 +107,14 @@ export default function LinkTreeView() {
     return (
         <div className="space-y-5">
             {devTreeLinks.map(item => (
-                <DevTreeInput 
+                <DevTreeInput
                     key={item.name}
                     item={item}
                     handleUrlChange={handleUrlChange}
                     handleEnableLink={handleEnableLink}
                 />
             ))}
-            <button 
+            <button
                 className="bg-cyan-400 p-2 text-lg w-full text-slate-600 rounded-lg font-bold"
                 onClick={() => mutate(user)}
             >Guardar Cambios</button>
