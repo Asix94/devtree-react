7bfbdd1bfdf9a0ac9ac0a0952d8386f99abdbd99
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-23 11:22:04 +0200

    creando un componente re-utilizable para el logo y navegaciones

--- src/components/nav/HomeNavigation.tsx
+++ src/components/nav/HomeNavigation.tsx
@@ -7,6 +7,11 @@ export default function HomeNavigation() {
                 className='text-white p-2 uppercase font-black text-xs cursor-pointer'
                 to='/auth/login'
             >Iniciar Sesión</Link>
+
+            <Link
+                className='bg-lime-500 text-slate-800 p-2 uppercase font-black text-xs cursor-pointer rounded-lg'
+                to='/auth/register'
+            >Registrarme</Link>
         </>
     )
 }
